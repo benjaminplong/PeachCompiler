beda654cb545a41a6cf5ca15d30d23c1cd2d4361
Ben Long <benjaminplong@gmail.com>
2024-04-19 17:28:39 -0400

    init parser structs

--- compiler.h
+++ compiler.h
@@ -4,12 +4,12 @@
 #include <stdbool.h>
 #include <string.h>
 #define S_EQ(str, str2) \
-          (str && str2 && (strcmp(str, str2) == 0))
+  (str && str2 && (strcmp(str, str2) == 0))
 struct pos
 {
   int line;
   int col;
-  const char* filename;
+  const char *filename;
 };
 
 #define NUMERIC_CASE \
@@ -53,7 +53,6 @@ struct pos
   case ')':         \
   case ']'
 
-
 enum
 {
   LEXICAL_ANALYSIS_ALL_OK,
@@ -86,22 +85,22 @@ struct token
   union
   {
     char cval;
-    const char* sval;
+    const char *sval;
     unsigned int inum;
     unsigned long lnum;
     unsigned long long llnum;
-    void* any;
+    void *any;
   };
   struct token_number
   {
     int type;
-  }num;
+  } num;
   // TRUE = whitespace between token and next token
   bool whitespace;
 
-  //pointer to the start of the bracket the token is between
-  //for debugging
-  const char* between_brackets;
+  // pointer to the start of the bracket the token is between
+  // for debugging
+  const char *between_brackets;
 };
 enum
 {
@@ -109,9 +108,9 @@ enum
   COMPILER_FAILED_WITH_ERRORS
 };
 struct lex_process;
-typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process* process);
-typedef char (*LEX_PROCESS_PEEK_CHAR)(struct lex_process* process);
-typedef void (*LEX_PROCESS_PUSH_CHAR)(struct lex_process* process, char c);
+typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process *process);
+typedef char (*LEX_PROCESS_PEEK_CHAR)(struct lex_process *process);
+typedef void (*LEX_PROCESS_PUSH_CHAR)(struct lex_process *process, char c);
 struct lex_process_functions
 {
   LEX_PROCESS_NEXT_CHAR next_char;
@@ -121,51 +120,109 @@ struct lex_process_functions
 struct lex_process
 {
   struct pos pos;
-  struct vector* token_vec;
-  struct compile_process* compiler;
+  struct vector *token_vec;
+  struct compile_process *compiler;
 
   /**
    * Count of how many brackets there are
    * ((50)) == count 2
-  */
+   */
   int current_expression_count;
-  struct buffer* parentheses_buffer;
-  struct lex_process_functions* functions;
+  struct buffer *parentheses_buffer;
+  struct lex_process_functions *functions;
 
-  //this will be private data the lexer does not understand, but the person using the lexer does
-  void* private;
+  // this will be private data the lexer does not understand, but the person using the lexer does
+  void *private;
 };
 struct compile_process
 {
-  //Compiler flags for how the file should be compiled
+  // Compiler flags for how the file should be compiled
   int flags;
 
   struct pos pos;
   struct compile_process_input_file
   {
-    FILE* fp;
-    const char* abs_path;
-  }cfile;
+    FILE *fp;
+    const char *abs_path;
+  } cfile;
 
   // a vector of tokens from lexical analysis
-  struct vector* token_vec;
-  FILE* ofile;
+  struct vector *token_vec;
+  FILE *ofile;
 };
-int compile_file(const char* filename, const char* out_filename, int flags);
-struct compile_process* compiler_process_create(const char* filename, const char* filename_out, int flags);
-char compile_process_next_char(struct lex_process* lex_process);
-char compile_process_peek_char(struct lex_process* lex_process);
-void compile_process_push_char(struct lex_process* lex_process, char c);
-struct lex_process* lex_process_create( struct compile_process* compiler, struct lex_process_functions* functions, void* private);
-void lex_process_free(struct lex_process* process);
-void* lex_process_private(struct lex_process* process);
-struct vector* lex_process_tokens(struct lex_process* process);
-int lex( struct lex_process* process);
-void compiler_error(struct compile_process* compiler, const char * msg, ...);
-void compiler_warning(struct compile_process* compiler, const char * msg, ...);
-bool token_is_keyword(struct token* token, const char* value);
+
+enum
+{
+  NODE_TYPE_EXPRESSION,
+  NODE_TYPE_EXPRESSION_PARENTHESES,
+  NODE_TYPE_NUMBER,
+  NODE_TYPE_IDENTIFIER,
+  NODE_TYPE_VARIABLE,
+  NODE_TYPE_VARIABLE_LIST,
+  NODE_TYPE_FUNCTION,
+  NODE_TYPE_BODY,
+  NODE_TYPE_STATEMENT_RETURN,
+  NODE_TYPE_STATEMENT_IF,
+  NODE_TYPE_STATEMENT_ELSE,
+  NODE_TYPE_STATEMENT_WHILE,
+  NODE_TYPE_STATEMENT_DO_WHILE,
+  NODE_TYPE_STATEMENT_FOR,
+  NODE_TYPE_STATEMENT_BREAK,
+  NODE_TYPE_STATEMENT_CONTINUE,
+  NODE_TYPE_STATEMENT_SWITCH,
+  NODE_TYPE_STATEMENT_CASE,
+  NODE_TYPE_STATEMENT_DEFAULT,
+  NODE_TYPE_STATEMENT_GOTO,
+
+  NODE_TYPE_UNARY,
+  NODE_TYPE_TENARY,
+  NODE_TYPE_LABEL,
+  NODE_TYPE_STRUCT,
+  NODE_TYPE_UNION,
+  NODE_TYPE_BRACKET,
+  NODE_TYPE_CAST,
+  NODE_TYPE_BLANK
+};
+
+struct node
+{
+  int type;
+  int flags;
+
+  struct pos;
+
+  struct node_binded
+  {
+    // pointer to body node this is in
+    struct node *owner;
+    // pointer to the function this node is in
+    struct node *function;
+  } binded;
+
+  union
+  {
+    char cval;
+    const char *sval;
+    unsigned int inum;
+    unsigned long lnum;
+    unsigned long long llnum;
+  };
+};
+int compile_file(const char *filename, const char *out_filename, int flags);
+struct compile_process *compiler_process_create(const char *filename, const char *filename_out, int flags);
+char compile_process_next_char(struct lex_process *lex_process);
+char compile_process_peek_char(struct lex_process *lex_process);
+void compile_process_push_char(struct lex_process *lex_process, char c);
+struct lex_process *lex_process_create(struct compile_process *compiler, struct lex_process_functions *functions, void *private);
+void lex_process_free(struct lex_process *process);
+void *lex_process_private(struct lex_process *process);
+struct vector *lex_process_tokens(struct lex_process *process);
+int lex(struct lex_process *process);
+void compiler_error(struct compile_process *compiler, const char *msg, ...);
+void compiler_warning(struct compile_process *compiler, const char *msg, ...);
+bool token_is_keyword(struct token *token, const char *value);
 /**
  * Builds tokens for the input string
-*/
-struct lex_process* tokens_build_for_string(struct compile_process* compiler, const char* str);
+ */
+struct lex_process *tokens_build_for_string(struct compile_process *compiler, const char *str);
 #endif
